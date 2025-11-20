9bd65779db0be9fdb13dd7fc122fea4ee6a00935
minho60 <minho031@gmail.com>
2025-11-20 17:48:50 +0900

    1120

--- js/scrollMotion.js
+++ js/scrollMotion.js
@@ -8,7 +8,7 @@
             const speed = 1000,
                 easing = "easeOutExpo",
                 pos = 0,
-                offset = 3;
+                offset = 0.5;
 
             // 오른쪽 상단에 스크롤양과 창 높이 값 출력
             console.log(windowScrollTop + " , " + windowHeight);
@@ -17,11 +17,11 @@
             if (windowScrollTop > windowHeight / offset) {
                 //참일 경우
                 for (let i = 0; i < 4; i++) {
-                    $(".motion").eq(i).stop().animate({ top: pos, opacity: 1 }, speed * (i + 2) / 2, easing);
+                    $(".motion").eq(i).stop().animate({ top: pos, opacity: 1 }, (speed * (i + 1)) / 2, easing);
                 };
 
             } else {
                 //거짓일 경우
-                $(".motion").stop().animate({ top: pos + 1000, opacity: 1 }, speed, easing);
+                $(".motion").stop().animate({ top: pos + 600, opacity: 0 }, speed, easing);
             }
         });
